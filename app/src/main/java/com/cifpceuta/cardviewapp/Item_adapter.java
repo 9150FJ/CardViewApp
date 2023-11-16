ae3900723d7fe391304fa7560e3216cab6b48187
FJesus <9150@cifpceuta.es>
2023-11-16 17:24:07 +0100

    Initial commit

---
+++ app/src/main/java/com/cifpceuta/cardviewapp/Item_adapter.java
@@ -0,0 +1,72 @@
+package com.cifpceuta.cardviewapp;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class Item_adapter extends RecyclerView.Adapter<Item_adapter.ViewHolder> {
+    ArrayList<String> list_item;
+
+    int ordem=0;
+
+
+    public Item_adapter(){}
+    public Item_adapter(ArrayList<String> list_item) {
+        this.list_item=list_item;
+    }
+
+    @NonNull
+    @Override
+    public Item_adapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.element_layout,parent,false);
+        return new Item_adapter.ViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull Item_adapter.ViewHolder holder, int position) {
+        holder.bindData(list_item.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return list_item.size();
+    }
+
+   public class ViewHolder extends RecyclerView.ViewHolder{
+        TextView tvItem;
+        Button btnBorrar;
+
+        private Item_adapter adapter;
+
+        public ViewHolder(@NonNull View itemView){
+            super(itemView);
+
+            tvItem=itemView.findViewById(R.id.tvItem); //Crear textview
+            btnBorrar = itemView.findViewById(R.id.btnBorrar); //Crear boton en xml
+
+            btnBorrar.setOnClickListener(view -> {
+                String texto_item = list_item.get(getAdapterPosition());
+                Toast.makeText(itemView.getContext(), "Elemento eliminado "+texto_item,Toast.LENGTH_LONG).show();
+                list_item.remove(getAdapterPosition());
+                notifyItemRemoved(getAdapterPosition());
+            });
+
+
+
+        }
+
+       void bindData(final String item){
+           tvItem.setText(item);
+       }
+   }
+
+}
+
---
+++ app/src/main/java/com/cifpceuta/cardviewapp/MainActivity.java
@@ -0,0 +1,59 @@
+package com.cifpceuta.cardviewapp;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    ArrayList<String> elementos = new ArrayList<>();
+    RecyclerView recyclerView;
+    Toolbar toolbar;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        for (int i=0;i<20;i++){
+            elementos.add("Elemento: "+i);
+        }
+
+
+        Item_adapter adapter = new Item_adapter(elementos);
+        recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id=item.getItemId();
+        if (id==R.id.opcion1){
+
+        }
+        else if(id==R.id.opcion2){
+
+        }
+        else if(id==R.id.opcion3){
+
+        }
+        return true;
+    }
+}
